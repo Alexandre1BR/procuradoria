18ede7cdec008ca288d60cebd068f72289bddf53
BMasquio <brunomasquio@gmail.com>
2018-01-11 10:35:56 -0200

    Modified data type on Processos

--- database/migrations/2018_01_10_162326_create_table_processos.php
+++ database/migrations/2018_01_10_162326_create_table_processos.php
@@ -21,19 +21,19 @@ class CreateTableProcessos extends Migration
             $table->integer('origem_id')->unsigned();
             $table->string('origem_complemento');
             $table->timestamp('data_distribuicao');
-            $table->integer('acao_id');
-            $table->integer('relator_id');
+            $table->integer('acao_id')->unsigned();
+            $table->integer('relator_id')->unsigned();
             $table->text('apensos_obs');
-            $table->integer('juiz_id');
-            $table->integer('autor');
+            $table->integer('juiz_id')->unsigned();
+            $table->text('autor');
             $table->text('reu');
             $table->text('objeto');
             $table->text('merito');
             $table->text('liminar');
             $table->text('recurso');
-            $table->integer('procurador_id');
-            $table->integer('estagiario_id');
-            $table->integer('assessor_id');
+            $table->integer('procurador_id')->unsigned();
+            $table->integer('estagiario_id')->unsigned();
+            $table->integer('assessor_id')->unsigned();
             $table->string('tipo_meio');
 
             $table->timestamps();

a7112ac69972aa6f6d4a2c27e27f0856b889a92d
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-11 16:30:36 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => '/processos'], function() {
+Route::group(['prefix' => '/processos'], function () {
     Route::get('/', 'Processos@create')->name('processos.create');
 
     Route::post('/', 'Processos@store')->name('processos.store');

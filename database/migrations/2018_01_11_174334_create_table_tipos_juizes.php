c874953b65bc168baf2c61aba429b4096d809be1
BMasquio <brunomasquio@gmail.com>
2018-01-11 16:35:12 -0200

    Migrations for acoes, juizes e tipos_juizes

---
+++ database/migrations/2018_01_11_174334_create_table_tipos_juizes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTiposJuizes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipos_juizes', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('nome');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tipos_juizes');
+    }
+}

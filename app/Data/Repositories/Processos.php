717eb386d0848eb6f77a3b2c82755a517dd27ad3
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-17 18:10:22 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Data/Repositories/Processos.php
+++ app/Data/Repositories/Processos.php
@@ -2,11 +2,11 @@
 
 namespace App\Data\Repositories;
 
+use App\Data\Models\Processo;
+use App\Http\Requests\Processo as ProcessoRequest;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\Data\Models\Processo;
 use Illuminate\Support\Facades\DB;
-use App\Http\Requests\Processo as ProcessoRequest;
 
 class Processos
 {
@@ -40,36 +40,37 @@ class Processos
 
     /**
      * @param null|string $search
+     *
      * @return \Illuminate\Database\Eloquent\Collection|static[]
      */
     public function searchFromRequest($search = null)
     {
         $search = is_null($search)
             ? collect()
-            : collect(explode(' ', $search))->map(function($item) {
+            : collect(explode(' ', $search))->map(function ($item) {
                 return strtolower($item);
             });
 
         $columns = collect([
             'numero_judicial' => 'string',
-            'numero_alerj' => 'string',
-            'vara' => 'string',
+            'numero_alerj'    => 'string',
+            'vara'            => 'string',
             //'origem_complemento' => 'string,
-            'apensos_obs' => 'string',
-            'autor' => 'string',
-            'reu' => 'string',
-            'objeto' => 'string',
-            'merito' => 'string',
-            'liminar' => 'string',
-            'recurso' => 'string',
-            'tipo_meio' => 'string',
+            'apensos_obs'       => 'string',
+            'autor'             => 'string',
+            'reu'               => 'string',
+            'objeto'            => 'string',
+            'merito'            => 'string',
+            'liminar'           => 'string',
+            'recurso'           => 'string',
+            'tipo_meio'         => 'string',
             'data_distribuicao' => 'date',
         ]);
 
         $query = Processo::query();
 
-        $search->each(function($item) use ($columns, $query) {
-            $columns->each(function($type, $column) use ($query, $item) {
+        $search->each(function ($item) use ($columns, $query) {
+            $columns->each(function ($type, $column) use ($query, $item) {
                 if ($type === 'string') {
                     $query->orWhere(DB::raw("lower({$column})"), 'like', '%'.$item.'%');
                 } else {

ac76b7300cc5c303e37ad5c2f11892e42cceeb96
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-17 14:37:15 -0200

    Refactor home menu

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 Auth::routes();
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
 
-require __DIR__.'/home.php';
+require __DIR__.'/services/home.php';
 
-require __DIR__.'/processos.php';
+require __DIR__.'/services/processos.php';
 
-require __DIR__.'/tribunais.php';
+require __DIR__.'/services/tribunais.php';
 
-require __DIR__.'/acoes.php';
+require __DIR__.'/services/acoes.php';
 
-require __DIR__.'/andamentos.php';
+require __DIR__.'/services/andamentos.php';

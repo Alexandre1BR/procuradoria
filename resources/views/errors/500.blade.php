6bcd2291c3d1ac217aa8869267a59e4673382668
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-17 14:35:51 -0200

    Add error pages

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.centered')
+
+@section('contents')
+    Ocorreu um erro grave, os administradores do sistema foram avisados
+@stop
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,35 +41,7 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        <li><a href="{{ route('processos.create') }}">Processos</a></li>
-                        <li><a href="{{ route('andamentos.create') }}">Andamentos</a></li>
-                        <li><a href="{{ route('tribunais.create') }}">Tribunais</a></li>
-
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            {{--<li><a href="{{ route('register') }}">Register</a></li>--}}
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
+                        @include('home.partials.menu')
                     </ul>
                 </div>
             </div>

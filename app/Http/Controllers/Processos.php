cd799fbad382c6ed46b9f1c5847b4d7435a89059
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-18 20:13:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/Processos.php
+++ app/Http/Controllers/Processos.php
@@ -11,21 +11,25 @@ use App\Data\Models\User as ModelUser;
 use App\Data\Repositories\Processos as ProcessosRepository;
 use App\Http\Requests\Processo as ProcessoRequest;
 
-class Processos extends Controller {
-    public function create() {
+class Processos extends Controller
+{
+    public function create()
+    {
         return view(
                 'processos.create',
                 $this->getProcessosData()
         );
     }
 
-    public function store(ProcessoRequest $request, ProcessosRepository $repository) {
+    public function store(ProcessoRequest $request, ProcessosRepository $repository)
+    {
         $repository->createFromRequest($request);
 
         return $this->create();
     }
 
-    public function detail(ProcessoRequest $request) {
+    public function detail(ProcessoRequest $request)
+    {
         $processo = Processo::find($request->id);
 
         return view('processos.detail')->with(array_merge(['processo'=>$processo], $this->getProcessosData()));
@@ -34,13 +38,14 @@ class Processos extends Controller {
     /**
      * @return array
      */
-    public function getProcessosData(): array {
+    public function getProcessosData(): array
+    {
         return [
-                'juizes' => ModelJuiz::orderBy('nome')->pluck('nome', 'id'),
+                'juizes'    => ModelJuiz::orderBy('nome')->pluck('nome', 'id'),
                 'tribunais' => ModelTribunal::orderBy('nome')->pluck('nome', 'id'),
-                'usuarios' => ModelUser::orderBy('name')->pluck('name', 'id'),
-                'meios' => ModelMeio::orderBy('nome')->pluck('nome', 'id'),
-                'acoes' => ModelAcao::orderBy('nome')->pluck('nome', 'id'),
+                'usuarios'  => ModelUser::orderBy('name')->pluck('name', 'id'),
+                'meios'     => ModelMeio::orderBy('nome')->pluck('nome', 'id'),
+                'acoes'     => ModelAcao::orderBy('nome')->pluck('nome', 'id'),
         ];
     }
 }

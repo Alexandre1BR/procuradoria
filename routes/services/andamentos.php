3519487fd4104c649f20240fa14be06365ac072f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-19 19:32:31 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- routes/services/andamentos.php
+++ routes/services/andamentos.php
@@ -1,8 +1,6 @@
 <?php
 
 Route::group(['prefix' => '/andamentos'], function () {
-
-
     Route::get('/create', 'Andamentos@create')->name('andamentos.create');
 
     Route::post('/', 'Andamentos@store')->name('andamentos.store');

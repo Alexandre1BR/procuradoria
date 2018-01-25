11782d97fa11a5be7e8d501f0c5ccf9bb24e31ac
Orlando Vinícius Valença Ferreira da Silva <orlando.vinicius1987@gmail.com>
2018-01-25 13:22:55 +0000

    Processos

--- resources/views/processos/show.blade.php
+++ resources/views/processos/show.blade.php
@@ -282,33 +282,54 @@
                 </div>
             </form>
             <br />
-                Andamentos
+                Andamentos <a href="{{route('andamentos.create')}}">Novo Andamento</a>
             <div class="row">
-                <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                    <thead>
+                <div class="col-md-12">
+                    <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                        <thead>
 
-                    <tr>
-                        <th>Tipo de Andamento</th>
-                        <th>Tipo de Entrada</th>
-                        <th>Tipo de Prazo</th>
-                        <th>Data Prazo</th>
-                        <th>Observação</th>
-                    </tr>
-                    </thead>
-                    @forelse ($andamentos as $andamento)
                         <tr>
-                            <td><a href="{{ route('andamentos.show',['id' => $andamento->id]) }}">{{ $andamento->processo->numero_judicial }}</a></td>
-                            <td>{{ $andamento->tipoAndamento->nome }}</td>
-                            <td>{{ $andamento->tipoEntrada->nome }}</td>
-                            <td>{{ $andamento->tipoPrazo->nome }}</td>
-                            <td>{{ $andamento->data_prazo }}</td>
-                            <td>{{ $andamento->observacoes }}</td>
+                            <th>Tipo de Andamento</th>
+                            <th>Tipo de Entrada</th>
+                            <th>Tipo de Prazo</th>
+                            <th>Data Prazo</th>
+                            <th>Observação</th>
                         </tr>
-                    @empty
-                        <p>Nenhum processo encontrado</p>
-                    @endforelse
-                </table>
+                        </thead>
+                        @forelse ($andamentos as $andamento)
+                            <tr>
+
+                                <td><a href="{{ route('andamentos.show',['id' => $andamento->id]) }}">{{ $andamento->tipoAndamento->nome }}</a></td>
+                                <td>{{ $andamento->tipoEntrada->nome }}</td>
+                                <td>{{ $andamento->tipoPrazo->nome }}</td>
+                                <td>{{ $andamento->data_prazo }}</td>
+                                <td>{{ $andamento->observacoes }}</td>
+                            </tr>
+                        @empty
+                            <p>Nenhum Andamento encontrado</p>
+                        @endforelse
+                    </table>
+                </div>
             </div>
+                Apensos
+                <div class="row">
+                    <div class="col-md-12">
+                        <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                            <thead>
+
+                            <tr>
+                                <th>Processo</th>
+                            </tr>
+                            </thead>
+                            @forelse($apensos as $apenso)
+                                <tr>
+                                    <td>{{$apenso->processo_id}}</td>
+                                </tr>
+                            @empty
+                                <p>Nenhum Apenso encontrado</p>
+                            @endforelse
+                        </table>
+                    </div>
 
         </div>
     </div>

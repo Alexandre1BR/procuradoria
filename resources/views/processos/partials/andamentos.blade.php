2060ff98688d1838d2441b3fcaaa61c781b13671
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-26 16:06:53 -0200

    Move buttons to headers

--- resources/views/processos/partials/andamentos.blade.php
+++ resources/views/processos/partials/andamentos.blade.php
@@ -3,20 +3,21 @@
         <div class="row">
             <div class="col-md-3">
                 <h4>
-                    Andamentos
+                    <a href="{{ route('home.index') }}">Andamentos</a>
+
                 </h4>
+            </div>
+
+            <div class="col-md-9">
+                <a href="{{ route('andamentos.create') }}" class="btn btn-primary pull-right" onclick="f_editar()">
+                    <i class="fa fa-plus"></i>
+                    novo andamento
+                </a>
             </div>
         </div>
     </div>
 
     <div class="panel-body">
-        <form action="{{ route('andamentos.create_post') }}" method="POST">
-            {{ csrf_field() }}
-            <input type="hidden" name="processo_id" value = {{$processo->id}}>
-            Andamentos
-            <input type="submit" value="Novo Andamento">
-        </form>
-
         <div class="row">
             <div class="col-md-12">
                 <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
--- resources/views/processos/partials/apensos.blade.php
+++ resources/views/processos/partials/apensos.blade.php
@@ -1,18 +1,44 @@
 <div class="panel panel-default">
     <div class="panel-heading">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <h4>
-                    Apensos <input type="button" value="Criar Apenso" onclick="createApenso()">
+                    <a href="{{ route('home.index') }}">Andamentos</a>
                 </h4>
+            </div>
+
+            <div class="col-md-6">
+                <form class="form" id="form_apensar" name="form_apensar" action="{{ route('processos.apensar') }}" method="post">
+                    {{ csrf_field() }}
+                    <input name="processo_id" type="hidden" value="{{$processo->id}}">
+
+                    <div class="row">
+                        <div class="col-md-10">
+                            <select name="apensado_id" class="js-example-basic-single form-control" disabled="disabled" id="apensado_id">
+                                <option value="">Selecione</option>
+                                @foreach($processos as $key=>$processoApensado)
+                                    <option value="{{ $key }}">{{ $processoApensado }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="col-md-2">
+                            <button type="submit" class="btn btn-primary btn-block">
+                                <i class="fa fa-plus"></i>
+                                apensar
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 
+
+
     <div class="panel-body">
         <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
-
                 <tr>
                     <th>Processo</th>
                 </tr>
@@ -29,25 +55,11 @@
                 <p>Nenhum Apenso encontrado</p>
             @endforelse
         </table>
-
-        <form id="form_apensar" name="form_apensar" action="{{ route('processos.apensar') }}" method="post">
-            {{ csrf_field() }}
-            <select name="apensado_id" class="js-example-basic-single form-control" disabled="disabled" id="apensado_id">
-                <option value="">Selecione</option>
-                @foreach($processos as $key=>$processoApensado)
-                    <option value="{{ $key }}">{{ $processoApensado }}</option>
-                @endforeach
-            </select>
-            <input name="processo_id" type="hidden" value="{{$processo->id}}">
-            <input type="submit" value="Apensar Processo" >
-        </form>
     </div>
 </div>
     <script>
     function createApenso(){
         $('#form_apensar *').prop("disabled",false).prop("readonly",false);
     }
-
-
 </script>
 

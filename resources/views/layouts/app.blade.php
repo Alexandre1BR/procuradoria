df8611d05bdd872eaca27271530e98c0de5de292
Breno Trengrouse <brenot@gmail.com>
2018-01-31 15:45:21 +0000

    Start Styling - Home and Header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        <img class="logo" src="/images/procuradoria.svg">
                     </a>
                 </div>
 

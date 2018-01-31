873edeab70bd39e933d65cd398caf9feca84eef7
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-01-31 18:32:06 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Data/Scope/Processo.php
+++ app/Data/Scope/Processo.php
@@ -3,20 +3,19 @@
  * Created by PhpStorm.
  * User: afdsilva
  * Date: 31/01/2018
- * Time: 15:08
+ * Time: 15:08.
  */
 
 namespace App\Data\Scope;
 
-use Illuminate\Database\Eloquent\Scope;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
 
-class Processo implements Scope {
-
+class Processo implements Scope
+{
     public function apply(Builder $builder, Model $model)
     {
         $builder->where('data_arquivamento', '=', null);
     }
-
 }

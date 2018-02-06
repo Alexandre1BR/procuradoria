e34757560be48628972086e4f17fcd16e2df897d
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-02-06 14:54:33 -0200

    Revert "Start Styling - Home and Header"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row login-home">
+    <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>

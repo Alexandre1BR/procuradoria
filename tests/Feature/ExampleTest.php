1e32b381925085ad7a09fed35491bff18f0009d0
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-02-22 13:10:17 -0300

    Update test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,26 +2,17 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use WithoutMiddleware;
-
     public function testRedirectToLoginResponse()
     {
-        //$this->withoutMiddleware();
-        $response = $this->get('/');
-        dd($response);
         $this->get('/')->assertRedirect('/login');
     }
 
     public function testLoginResponse()
     {
-
-        //$this->withoutMiddleware();
-
         $response = $this->get('/login');
 
         $response->assertStatus(200);

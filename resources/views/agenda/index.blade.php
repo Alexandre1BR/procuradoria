0833e1f4e07536badf7d75f035dc612f7bde7881
Alexandre Ferreira <alexandreee@gmail.com>
2018-03-05 15:36:19 -0300

    Impressão de agenda

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="panel panel-default">
-        <div class="panel-heading">
+
+    <div class="panel panel-default ">
+        <div class="panel-heading hidden-print">
             <div class="row">
                 <div class="col-md-3">
                     <h4>Agenda</h4>
                 </div>
 
-                <div class="col-md-9">
+                <div class="col-md-9 hidden-print">
                     <div class="form-group pull-right">
                         <div class="row">
                             <div class="col-xs-12">
--- resources/views/agenda/partials/calendar.blade.php
+++ resources/views/agenda/partials/calendar.blade.php
@@ -1,1 +1,3 @@
+<a title='Imprimir conteúdo' href='javascript:window:print()' class="hidden-print "><i class="fa fa-print"></i> Imprimir</a>
+
 <div id="calendar"></div>

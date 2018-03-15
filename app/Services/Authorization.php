910b95452394bda548b354b2b16fb396645887f7
Alexandre Ferreira <alexandreee@gmail.com>
2018-03-15 14:17:58 -0300

    PHPDocs → Services

--- app/Services/Authorization.php
+++ app/Services/Authorization.php
@@ -6,14 +6,8 @@ use App\Services\Traits\RemoteRequest;
 
 class Authorization
 {
-    /**
-     *
-     */
     const PERMISSIONS_URL = 'http://apiportal.alerj.rj.gov.br/api/v1.0/adm-user/K7k8H95loFpTH0ZTRKX2BhADIusjXHInHW3cspyosOoNrbd5jOG3pd61F4d6fg584Gg5h4DSjui1k/permissions';
 
-    /**
-     *
-     */
     const SYSTEM_NAME = 'Procuradoria';
 
     /**

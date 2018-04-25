68974cc253f7c5d91b3fdea7747273cf2a5534ab
Alexandre Ferreira <alexandreee@gmail.com>
2018-04-25 18:22:26 +0000

    Fix code - Filtros no Select2 ajustados

--- public/js/app.js
+++ public/js/app.js
@@ -80481,6 +80481,13 @@ $(function () {
     $(document).ready(function () {
         $('.select2').select2({
             theme: "bootstrap",
+            tags: false,
+            width: "100%"
+            //minimumResultsForSearch: Infinity,
+        });
+
+        $('.select2-tag').select2({
+            theme: "bootstrap",
             tags: true,
             width: "100%"
         });
--- resources/assets/js/components/select2.js
+++ resources/assets/js/components/select2.js
@@ -6,6 +6,13 @@ $(() => {
     $(document).ready(function() {
         $('.select2').select2({
             theme: "bootstrap",
+            tags: false,
+            width: "100%",
+            //minimumResultsForSearch: Infinity,
+        });
+
+        $('.select2-tag').select2({
+            theme: "bootstrap",
             tags: true,
             width: "100%",
         });

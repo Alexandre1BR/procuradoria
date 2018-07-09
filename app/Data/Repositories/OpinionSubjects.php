a77d15a2fa79677755c2eb8215e8d7eeb342d105
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-07-09 15:26:36 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Data/Repositories/OpinionSubjects.php
+++ app/Data/Repositories/OpinionSubjects.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Data\Repositories;
 
 use App\Data\Models\OpinionSubject as OpinionSubjectsModel;
@@ -12,10 +13,10 @@ class OpinionSubjects extends Base
         $array = [];
 
         $array[] = (object) [
-            'name' => 'name',
-            'showName' => 'Nome do Assunto',
+            'name'       => 'name',
+            'showName'   => 'Nome do Assunto',
             'columnSize' => '100%',
-            'type' => 'string'
+            'type'       => 'string',
         ];
 
         return $array;
@@ -26,9 +27,9 @@ class OpinionSubjects extends Base
         $array = [];
 
         $array[] = (object) [
-            'name' => 'name',
+            'name'     => 'name',
             'showName' => 'Nome do Assunto',
-            'type' => 'string'
+            'type'     => 'string',
         ];
 
         return $array;
@@ -75,14 +76,14 @@ class OpinionSubjects extends Base
                         $query->orWhere(
                             DB::raw("lower({$column->name})"),
                             'like',
-                            '%' . $item . '%'
+                            '%'.$item.'%'
                         );
                         break;
                     case 'textarea':
                         $query->orWhere(
                             DB::raw("lower({$column->name})"),
                             'like',
-                            '%' . $item . '%'
+                            '%'.$item.'%'
                         );
                         break;
                     case 'id':
@@ -90,8 +91,8 @@ class OpinionSubjects extends Base
                             $query
                         ) use ($item, $column) {
                             $query->whereRaw(
-                                'lower(' .
-                                    $column->foreignName .
+                                'lower('.
+                                    $column->foreignName.
                                     ") like '%{$item}%'"
                             );
                         });

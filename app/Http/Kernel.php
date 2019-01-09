28ac2bee7b4e30b75612ad23e4ec333904b4b7e4
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-01-09 18:08:29 -0200

    Fix Kernel for Laravel 5.7

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,5 +55,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'app.users' => \App\Http\Middleware\MustBeAppUser::class,
         'app.subsystem' => \App\Http\Middleware\SelectSubsystem::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
